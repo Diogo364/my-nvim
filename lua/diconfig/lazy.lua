1f2c34a277a4c59de76daf14da3ce8b9668afdbc
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-05-21 18:40:31 -0400

    LSP configuration

--- lua/diconfig/lazy.lua
+++ lua/diconfig/lazy.lua
@@ -11,4 +11,16 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("diconfig.plugins")
+require("lazy").setup({
+    { import = "diconfig.plugins" },
+    { import = "diconfig.plugins.lsp" }
+},
+    {
+        checker = {
+            enabled = true,
+            notify = false,
+        },
+        change_detection = {
+            notify = false
+        }
+    })
