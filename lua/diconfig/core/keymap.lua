ab37ddc1d198cac38b8d760c0b252b881ce074db
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-05-22 14:54:58 -0400

    remove leader pv from shortcuts

--- lua/diconfig/core/keymap.lua
+++ lua/diconfig/core/keymap.lua
@@ -25,7 +25,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.forma()
+    vim.lsp.buf.format()
 end)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
