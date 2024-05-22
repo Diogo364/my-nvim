f19a1ca37503268aa45e7cc53a69e59cd1ff5951
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-05-22 17:52:52 -0400

    Remapping conflicting Enter keybinding

--- lua/diconfig/plugins/jukit.lua
+++ lua/diconfig/plugins/jukit.lua
@@ -1,7 +1,8 @@
 return {
     "luk400/vim-jukit",
-    ft = { "py" },
-    config = function() 
+    ft = { "python" },
+    config = function()
         vim.keymap.set("n", "<leader><cr>", ":call jukit#send#section(0)<cr>")
+        vim.keymap.set("n", "<Nop>", ":call jukit#send#line()<cr>")
     end
 }
