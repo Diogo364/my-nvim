37eb81968785c2246e72488443048e585f66f0ea
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-05-30 14:38:31 -0400

    Remove autoclose mapping in insert mode

--- lua/diconfig/core/keymap.lua
+++ lua/diconfig/core/keymap.lua
@@ -22,12 +22,6 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
--- Autoclose
-vim.keymap.set("i", "(", "()<Left>", { noremap = true })
-vim.keymap.set("i", "{", "{}<Left>", { noremap = true })
-vim.keymap.set("i", "'", "''<Left>", { noremap = true })
-vim.keymap.set("i", '"', '""<Left>', { noremap = true })
-
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
