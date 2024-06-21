7ba1dd961894c4409e1e03566d97d50ec0a482e4
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-06-21 10:54:54 -0400

    Add git diff and git log support

--- lua/diconfig/plugins/fugitive.lua
+++ lua/diconfig/plugins/fugitive.lua
@@ -1,6 +1,8 @@
 return {
     "tpope/vim-fugitive",
-    config = function() 
+    config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gdiff", ":Git diff<CR>")
+        vim.keymap.set("n", "<leader>glog",  ":Git log<CR>")
     end
 }
