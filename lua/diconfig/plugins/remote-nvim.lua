955ee6db4db45a6097f06b9bccc65ec4d4fc0041
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-06-21 08:29:43 -0400

    Update remote-nvim version to v3.9.0

--- lua/diconfig/plugins/remote-nvim.lua
+++ lua/diconfig/plugins/remote-nvim.lua
@@ -1,6 +1,6 @@
 return {
     "amitds1997/remote-nvim.nvim",
-    version = "v0.2.7",                 -- Pin to GitHub releases
+    version = "v0.3.9",                 -- Pin to GitHub releases
     dependencies = {
         "nvim-lua/plenary.nvim",   -- For standard functions
         "MunifTanjim/nui.nvim",    -- To build the plugin UI
