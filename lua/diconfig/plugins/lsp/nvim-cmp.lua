6aef9a24a050b48afe2742d661dd5b0b196e9896
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-08-21 08:23:39 -0400

    Update LSP commands to default

--- lua/diconfig/plugins/lsp/nvim-cmp.lua
+++ lua/diconfig/plugins/lsp/nvim-cmp.lua
@@ -29,13 +29,19 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<A-k>"] = cmp.mapping.select_prev_item(),
-                ["<A-j>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                ["<C-y>"] = cmp.mapping(
+                    cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                    { "i", "c" }
+                ),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
