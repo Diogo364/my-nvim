7b6991798e8d8f0fb1b8157ea86a67d6ce0f033e
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-08-21 08:17:23 -0400

    Jukit variables definition within init function

--- lua/diconfig/plugins/jukit.lua
+++ lua/diconfig/plugins/jukit.lua
@@ -1,18 +1,19 @@
--- Jukit Global Config
-vim.g.jukit_mappings = 0
-vim.g.jukit_layout = {
-	p1 = 0.65,
-	split = "vertical",
-	val = {
-		"file_content",
-		{
-			split = "horizontal",
-			p1 = 0.6,
-			val = { "output", "output_history" },
-		},
-	},
-}
-
 return {
 	"luk400/vim-jukit",
+	init = function()
+		-- Jukit Global Config
+		vim.g.jukit_mappings = 0
+		vim.g.jukit_layout = {
+			p1 = 0.65,
+			split = "vertical",
+			val = {
+				"file_content",
+				{
+					split = "horizontal",
+					p1 = 0.6,
+					val = { "output", "output_history" },
+				},
+			},
+		}
+	end,
 }
