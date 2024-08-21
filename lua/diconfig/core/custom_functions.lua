bd723c38110b6824bfd6ca4f612ea6ec43c599de
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-08-21 08:12:20 -0400

    Add custom function to print lua tables

---
+++ lua/diconfig/core/custom_functions.lua
@@ -0,0 +1,4 @@
+P = function (table_object)
+    print(vim.inspect(table_object))
+    return table_object
+end
--- lua/diconfig/core/init.lua
+++ lua/diconfig/core/init.lua
@@ -1,2 +1,3 @@
 require("diconfig.core.keymap")
 require("diconfig.core.set_config")
+require("diconfig.core.custom_functions")
