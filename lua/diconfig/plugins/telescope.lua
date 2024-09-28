7b0f88e9e02e184a16dd16c36aad325300e15b5d
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-09-28 16:17:22 -0400

    Update telescope version

--- lua/diconfig/plugins/telescope.lua
+++ lua/diconfig/plugins/telescope.lua
@@ -1,13 +1,13 @@
 return {
     "nvim-telescope/telescope.nvim",
-
-    tag = "0.1.6",
-
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
     },
     config = function()
         require("diconfig.plugins.custom-telescope.setup")
+        require("telescope").load_extension("ui-select")
 
     end,
 }
