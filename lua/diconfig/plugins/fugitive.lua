e160880915c3530bbf285547d1428ed70069bf76
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-10-16 14:14:40 -0400

    Remove unnecessary plugins

--- lua/diconfig/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open Git status window" })
-        vim.keymap.set("n", "<leader>gdiff", ":Git diff<CR>", { desc = "Show Git diff" })
-        vim.keymap.set("n", "<leader>glog", ":Git log<CR>", { desc = "Show Git log" })
-    end,
-}
--- lua/diconfig/plugins/lsp/mason.lua
+++ lua/diconfig/plugins/lsp/mason.lua
@@ -23,32 +23,14 @@ return {
         })
         mason_lspconfig.setup({
             --list of servers for mason to install
-            ensure_installed = {
-                "bashls",
-                "docker_compose_language_service",
-                "dockerls",
-                "gopls",
-                "jsonls",
-                "lua_ls",
-                "markdown_oxide",
-                "pyright",
-                "rust_analyzer",
-                "html",
-                "cssls",
-            },
+            ensure_installed = {},
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
         })
         -- import mason-tool-installer
         local mason_tool_installer = require("mason-tool-installer")
         mason_tool_installer.setup({
-            ensure_installed = {
-                "black",
-                "flake8",
-                "isort",
-                "pylint",
-                "debugpy",
-            },
+            ensure_installed = {},
         })
     end,
 }
