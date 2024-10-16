bed9e456dc8bc4502bd938eb33ebd51aff47e299
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-10-16 13:30:09 -0400

    Completion updates

--- lua/diconfig/plugins/lsp/nvim-cmp.lua
+++ lua/diconfig/plugins/lsp/nvim-cmp.lua
@@ -3,17 +3,55 @@ return {
     event = "InsertEnter",
     dependencies = {
         "hrsh7th/cmp-buffer",     -- source for text in buffer
+        "hrsh7th/cmp-cmdline",    -- source cmdline
         "hrsh7th/cmp-path",       -- source for file system paths
         "L3MON4D3/LuaSnip",       -- snippet engine
-        "saadparwaiz1/cmp_luasnip", -- for autocompletion
         "rafamadriz/friendly-snippets", -- useful snippets
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
     },
     config = function()
         -- If you want insert `(` after select function or method item
         local cmp = require("cmp")
 
-
         local luasnip = require("luasnip")
+
+        local kind_icons = {
+            Array = " ",
+            Boolean = " ",
+            Class = " ",
+            Color = " ",
+            Constant = " ",
+            Constructor = " ",
+            Copilot = " ",
+            Enum = " ",
+            EnumMember = " ",
+            Event = " ",
+            Field = " ",
+            File = " ",
+            Folder = " ",
+            Function = " ",
+            Interface = " ",
+            Key = " ",
+            Keyword = " ",
+            Method = " ",
+            Module = " ",
+            Namespace = " ",
+            Null = " ",
+            Number = " ",
+            Object = " ",
+            Operator = " ",
+            Package = " ",
+            Property = " ",
+            Reference = " ",
+            Snippet = " ",
+            String = " ",
+            Struct = " ",
+            Text = " ",
+            TypeParameter = " ",
+            Unit = " ",
+            Value = " ",
+            Variable = " ",
+        }
 
         -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
         require("luasnip.loaders.from_vscode").lazy_load()
@@ -22,11 +60,22 @@ return {
                 completeopt = "menu,menuone,preview,noselect",
             },
             mapping = cmp.mapping.preset.insert({
+                ["<C-g>"] = function()
+                    if not cmp.visible() then
+                        cmp.complete()
+                    end
+                end,
                 ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-n>"] = function()
+                    if cmp.visible() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                    else
+                        cmp.complete()
+                    end
+                end,
+                ["<C-h>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-l>"] = cmp.mapping.scroll_docs(4),
+                ["<C-c>"] = cmp.mapping.abort(),
                 ["<C-y>"] = cmp.mapping(
                     cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Insert,
@@ -39,13 +88,81 @@ return {
                 { name = "nvim_lsp" },
                 { name = "luasnip" }, -- snippets
                 { name = "buffer" }, -- text within current buffer
-                { name = "path" }, -- file system paths
+                -- file system paths
+                {
+                    name = "path",
+                    option = {
+                        label_trailing_slash = true,
+                        get_cwd = function()
+                            return vim.fn.getcwd()
+                        end,
+                    },
+                },
             }),
             snippet = {
                 expand = function(args)
                     vim.snippet.expand(args.body)
                 end,
             },
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                    vim_item.menu = ({
+                        buffer = "Buffer",
+                        luasnip = "Snippet",
+                        nvim_lsp = "LSP",
+                        path = "Path",
+                        spell = "Spell",
+                    })[entry.source.name]
+                    return vim_item
+                end,
+            },
+            sorting = {
+                priority_weight = 1.0,
+                comparators = {
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.sort_text,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
+            },
+        })
+
+        -- `/` cmdline setup.
+        cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer" },
+            },
+            priority_weight = 1.0,
+            comparators = {
+                cmp.config.compare.offset,
+                cmp.config.compare.exact,
+                cmp.config.compare.score,
+                cmp.config.compare.kind,
+                cmp.config.compare.sort_text,
+                cmp.config.compare.length,
+                cmp.config.compare.order,
+            },
+        })
+
+        -- `:` cmdline setup.
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                {
+                    name = "cmdline",
+                    option = {
+                        ignore_cmds = { "Man", "!" },
+                    },
+                },
+            }),
         })
     end,
 }
