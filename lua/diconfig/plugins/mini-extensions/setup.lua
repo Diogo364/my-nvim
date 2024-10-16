c802449bdc9b2b6c737d4b634808d8e1ec6429c9
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-10-16 13:28:45 -0400

    initial mini config

---
+++ lua/diconfig/plugins/mini-extensions/setup.lua
@@ -0,0 +1,5 @@
+local ai_config = require("diconfig.plugins.mini-extensions.ai")
+local align_config = require("diconfig.plugins.mini-extensions.align")
+
+require("mini.ai").setup(ai_config)
+require("mini.align").setup(align_config)
---
+++ lua/diconfig/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function ()
+        require("diconfig.plugins.mini-extensions.setup")
+    end
+}
