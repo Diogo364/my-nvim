50530e07468917d68e99715ede1e19a495884b51
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-10-16 13:30:32 -0400

    Colorscheme manager sync-autocmd branch settings

--- lua/diconfig/plugins/colorscheme-manager.lua
+++ lua/diconfig/plugins/colorscheme-manager.lua
@@ -2,18 +2,20 @@ return {
 	-- dir = "/home/dinascimento/Documents/myProjects/custom_nvim_lua_plugs/colorscheme_telescope.nvim",
 	-- name = "colorscheme-manager",
 	"Diogo364/colorscheme-manager.nvim",
-	tag = "beta",
+	branch = "sync-autocmd",
 	config = function()
 		local ColorMyPencils = function()
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+			vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end
 
 		require("colorscheme-manager").setup({
-			colorscheme = "tokyonight-night",
+			colorscheme = "rose-pine-moon",
 			enable_custom_options = true,
 			custom_options_function = ColorMyPencils,
+			autocmd = true,
 		})
 	end,
 }
