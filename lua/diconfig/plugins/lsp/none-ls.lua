5bc452ff4ffb0509b9b25194d16dfc854d7cdf46
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-11-05 09:00:46 -0400

    Add shfmt for shell formatting

--- lua/diconfig/plugins/lsp/none-ls.lua
+++ lua/diconfig/plugins/lsp/none-ls.lua
@@ -1,15 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-			},
-			degub = true,
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- Formatting
+                --- Lua
+                null_ls.builtins.formatting.stylua,
+                --- Python
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                --- Shell
+                null_ls.builtins.formatting.shfmt.with({
+                    filetypes = { "sh", "bash", "zsh" },
+                }),
+            },
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code using LSP" })
 	end,
