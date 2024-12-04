a4aa01502abaa3e5db2608225bc87effd55e4f9a
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-04 09:02:07 -0400

    Lazyload lsp configs

--- lua/diconfig/plugins/lsp/lspconfig.lua
+++ lua/diconfig/plugins/lsp/lspconfig.lua
@@ -1,5 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
+    lazy = true,
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
@@ -179,6 +180,34 @@ return {
         lspconfig["lua_ls"].setup({
             on_attach = on_attach,
             capabilities = capabilities,
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        vim.uv.fs_stat(path .. "/.luarc.json")
+                        or vim.uv.fs_stat(path .. "/.luarc.jsonc")
+                    then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua =
+                    vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = "LuaJIT",
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME,
+                                vim.fn.stdpath("config"),
+                            },
+                        },
+                    })
+            end,
             settings = {
                 Lua = {
                     runtime = { version = "Lua 5.1" },
--- lua/diconfig/plugins/lsp/none-ls.lua
+++ lua/diconfig/plugins/lsp/none-ls.lua
@@ -1,29 +1,24 @@
 return {
     "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                -- Formatting
-                --- Lua
-                null_ls.builtins.formatting.stylua,
-                --- Python
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-                --- Shell
-                null_ls.builtins.formatting.shfmt.with({
-                    filetypes = { "sh", "bash", "zsh" },
-                }),
-            },
-
-            degub = true,
-        })
-
+    event = "VeryLazy",
+    opts = function()
         vim.keymap.set(
             "n",
             "<leader>gf",
             vim.lsp.buf.format,
             { desc = "Format code using LSP" }
         )
+
+        return {
+            sources = {
+                require("null-ls").builtins.formatting.stylua,
+                require("null-ls").builtins.formatting.black,
+                require("null-ls").builtins.formatting.isort,
+                require("null-ls").builtins.formatting.shfmt.with({
+                    filetypes = { "sh", "bash", "zsh" },
+                }),
+            },
+            degub = true,
+        }
     end,
 }
