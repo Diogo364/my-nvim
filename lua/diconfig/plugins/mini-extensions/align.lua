5a677bbaf2bc67ec6ec1209b2e7ca5c8dfed052a
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-04 09:07:06 -0400

    Fix formatting for mini

--- lua/diconfig/plugins/mini-extensions/align.lua
+++ lua/diconfig/plugins/mini-extensions/align.lua
@@ -1,20 +1,13 @@
--- local MiniAlign = require("mini.align")
-
 M = {
-    -- Module mappings. Use `''` (empty string) to disable one.
     mappings = {
         start = "ga",
         start_with_preview = "gA",
     },
-
-    -- Default options controlling alignment process
     options = {
         split_pattern = "",
         justify_side = "left",
         merge_delimiter = "",
     },
-
-    -- Default steps performing alignment (if `nil`, default is used)
     steps = {
         pre_split = {},
         split = nil,
@@ -23,8 +16,6 @@ M = {
         pre_merge = {},
         merge = nil,
     },
-
-    -- Whether to disable showing non-error feedback
     silent = false,
 }
 
