9ca67d49dc5fb539e87e51f16575eb33e10b0210
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-04 09:12:38 -0400

    Update my custom prompts

--- lua/diconfig/plugins/llm/config/prompts.lua
+++ lua/diconfig/plugins/llm/config/prompts.lua
@@ -1,68 +1,68 @@
 local config = require("diconfig.plugins.llm.config.core")
 
 return {
-    Generate = {
-        prompt = "$input",
-        model = config.models.chat_model,
-        replace = true,
-    },
-    Chat = {
+    ["Generate"] = {
         prompt = "$input",
         model = config.models.chat_model,
     },
-    Summarize = {
+    ["Summarize"] = {
         prompt = "Summarize the following text:\n$text",
         model = config.models.chat_model,
     },
-    Change = {
+    ["Change"] = {
         prompt = "Change the following text, $input, just output the final text without additional quotes around it:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Enhance_Grammar_Spelling = {
+    ["Enhance_Grammar_Spelling"] = {
         prompt = "Modify the following text to improve grammar and spelling, just output the final text without additional quotes around it:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Enhance_Wording = {
+    ["Enhance_Wording"] = {
         prompt = "Modify the following text to use better wording, just output the final text without additional quotes around it:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Make_Concise = {
+    ["Make_Concise"] = {
         prompt = "Modify the following text to make it as simple and concise as possible, just output the final text without additional quotes around it:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Make_List = {
+    ["Make_List"] = {
         prompt = "Render the following text as a markdown list:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Make_Table = {
+    ["Make_Table"] = {
         prompt = "Render the following text as a markdown table:\n$text",
         model = config.models.chat_model,
         replace = true,
     },
-    Review_Code = {
+    ["Review_Code"] = {
         prompt = "Review the following code and make concise suggestions:\n```$filetype\n$text\n```",
         model = config.models.code_model,
     },
-    Enhance_Code = {
+    ["Enhance_Code"] = {
         prompt = "Enhance the following code, only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
         model = config.models.code_model,
         replace = true,
         extract = "```$filetype\n(.-)```",
     },
-    Change_Code = {
+    ["Change_Code"] = {
         prompt = "Regarding the following code, $input, only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
         model = config.models.code_model,
         replace = true,
         extract = "```$filetype\n(.-)```",
     },
-    Create_Code = {
+    ["Create_Code"] = {
         model = config.models.code_model,
-        prompt = "Create a code that is able to, $input, only output the result in format ```$filetype\n...\n```:\n```$filetype\n```",
+        prompt = "Create a code that is able to, $input, only output the result in format ```$filetype\n...\n```",
+        extract = "```$filetype\n(.-)```",
+    },
+    ["Create_Code_Selection"] = {
+        model = config.models.code_model,
+        prompt = "Create a code that is able to, $text, only output the result in format ```$filetype\n...\n```",
         extract = "```$filetype\n(.-)```",
     },
 }
