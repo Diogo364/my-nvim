b6380c56fd078a96eea806710ce0351a73c5c9ef
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-01-02 14:37:01 -0400

    New shortcuts to run lua within neovim jit

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,13 +1,27 @@
 vim.keymap.set(
     "n",
     "<leader>x",
+    "<CMD>w<CR><CMD>.lua<CR>",
+    { buffer = true, silent = true, desc = "Source current line" }
+)
+
+vim.keymap.set(
+    "v",
+    "<leader>x",
+    ":lua<CR>",
+    { buffer = true, silent = true, desc = "Run Selected Lines" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader><leader>x",
     "<CMD>w<CR><CMD>source %<CR>",
     { buffer = true, silent = true, desc = "Source current lua file" }
 )
 
 vim.keymap.set(
     "n",
-    "<leader><space>t",
+    "<leader><leader>t",
     "<CMD>w<CR><Plug>PlenaryTestFile<CR>",
     { buffer = true, desc = "Run Plenary test on current file" }
 )
