965ad05772d75856d9ff352c152dd96485434546
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-01-02 14:37:27 -0400

    Add Floke's lazydev plugin

--- lua/diconfig/plugins/lsp/lspconfig.lua
+++ lua/diconfig/plugins/lsp/lspconfig.lua
@@ -5,6 +5,15 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         { "antosha417/nvim-lsp-file-operations", config = true },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
         { "nvim-telescope/telescope.nvim" },
     },
     config = function()
