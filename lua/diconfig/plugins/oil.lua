562bc9a5ec409f91d7b1c0844c4ebc05794e6797
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-05-29 08:54:15 -0400

    Replace oil icons

--- lua/diconfig/plugins/oil.lua
+++ lua/diconfig/plugins/oil.lua
@@ -32,7 +32,7 @@ end
 
 return {
     "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "echasnovski/mini.icons" },
     opts = function()
         vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open Oil" })
         return {
