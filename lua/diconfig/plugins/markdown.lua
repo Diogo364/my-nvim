d590240693f2b653f7f697209412aa3aead386a6
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-05-29 08:54:01 -0400

    Add multiple markdown settups

--- lua/diconfig/plugins/markdown.lua
+++ lua/diconfig/plugins/markdown.lua
@@ -1,9 +1,27 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    lazy = true,
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
+    {
+        "iamcco/markdown-preview.nvim",
+        lazy = true,
+        cmd = {
+            "MarkdownPreviewToggle",
+            "MarkdownPreview",
+            "MarkdownPreviewStop",
+        },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        enabled = true,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "echasnovski/mini.nvim",
+        },
+        ft = { "markdown", "codecompanion" },
+        opts = {
+            completions = { blink = { enabled = true } },
+        },
+    },
 }
