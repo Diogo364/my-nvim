ffcb4a95dfd8d67c4ab7b38a15b3e42a0edaf49e
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-05-29 08:53:14 -0400

    Remove Mini.align

--- lua/diconfig/plugins/mini-extensions/setup.lua
+++ lua/diconfig/plugins/mini-extensions/setup.lua
@@ -1,5 +1,3 @@
 local ai_config = require("diconfig.plugins.mini-extensions.ai")
-local align_config = require("diconfig.plugins.mini-extensions.align")
 
 require("mini.ai").setup(ai_config)
-require("mini.align").setup(align_config)
