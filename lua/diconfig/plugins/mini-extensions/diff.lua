e8f56bd00b9807360c795e01778735537eb4da3b
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-05-29 08:53:21 -0400

    Add Mini.diff

---
+++ lua/diconfig/plugins/mini-extensions/diff.lua
@@ -0,0 +1,7 @@
+local gen_source = require("mini.diff").gen_source.none()
+
+M = {
+    source = gen_source,
+}
+
+return M
--- lua/diconfig/plugins/mini-extensions/setup.lua
+++ lua/diconfig/plugins/mini-extensions/setup.lua
@@ -1,3 +1,5 @@
 local ai_config = require("diconfig.plugins.mini-extensions.ai")
+local diff_config = require("diconfig.plugins.mini-extensions.diff")
 
 require("mini.ai").setup(ai_config)
+require("mini.diff").setup(diff_config)
