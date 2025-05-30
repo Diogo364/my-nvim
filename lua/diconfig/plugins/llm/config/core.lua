db37f7b1168631c306f10f8e3bf14737a948187b
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-05-30 15:02:26 -0400

    Export llm config to env variables

--- lua/diconfig/plugins/llm/config/core.lua
+++ lua/diconfig/plugins/llm/config/core.lua
@@ -1,14 +1,15 @@
 M = {}
 
 local setup = {}
-setup.host = "localhost"
-setup.port = "11434"
+local env = vim.fn.environ()
+setup.host = env["NVIM_LLM_HOST"] or "localhost"
+setup.port = env["NVIM_LLM_PORT"] or "11434"
 setup.url = string.format("http://%s:%s", setup.host, setup.port)
 
 return {
     setup = setup,
     models = {
-        chat_model = "qwen2.5:3b-instruct",
-        code_model = "qwen2.5-coder:3b",
+        chat_model = env["NVIM_LLM_CHAT_MODEL"] or "qwen2.5:3b-instruct",
+        code_model = env["NVIM_LLM_CODE_MODEL"] or "qwen2.5-coder:3b",
     },
 }
