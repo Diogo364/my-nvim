feba84340752c84ef5a9a5fa5a18a71c12849092
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-10-08 22:13:52 -0300

    Update ref for colorscheme

--- lua/diconfig/plugins/colorscheme-manager.lua
+++ lua/diconfig/plugins/colorscheme-manager.lua
@@ -3,6 +3,7 @@ local ColorMyPencils = function()
     -- vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "Yellow" })
     vim.api.nvim_set_hl(0, "PmenuSel", { bg = "Yellow", fg = "Black" })
 end
 
@@ -10,10 +11,11 @@ return {
     -- dir = "/home/dinascimento/Documents/myProjects/custom_nvim_lua_plugs/colorscheme_telescope.nvim",
     -- name = "colorscheme-manager",
     "Diogo364/colorscheme-manager.nvim",
-    tag = "0.3.0",
+    -- tag = "0.3.0",
+    branch = "feature_cache_file",
     priority = 1000,
     opts = {
-        colorscheme = "rose-pine-moon",
+        colorscheme = "rose-pine",
         enable_custom_options = true,
         custom_options_function = ColorMyPencils,
         autocmd = true,
