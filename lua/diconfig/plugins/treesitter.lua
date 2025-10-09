72500a4a6acf889ce3e028f387e6fac7b3d5b16a
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-10-08 22:13:17 -0300

    more treesitter things

--- lua/diconfig/plugins/treesitter.lua
+++ lua/diconfig/plugins/treesitter.lua
@@ -4,11 +4,16 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "markdown",
+                "markdown_inline",
+            },
             auto_install = true,
             indent = {
                 enable = true,
             },
             highlight = {
+                enable = true,
                 additional_vim_regex_highlighting = { "markdown" },
                 disable = function(lang, buf)
                     local max_filesize = 100 * 1024 -- 100 KB
@@ -20,6 +25,15 @@ return {
                         return true
                     end
                 end,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
             },
         })
 
