6ba9ce74bff10f039fdbbd695ead747f65563040
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-10-08 22:02:19 -0300

    Command for redirect output to buffer

--- lua/diconfig/custom-plugins/utils.lua
+++ lua/diconfig/custom-plugins/utils.lua
@@ -1,3 +1,16 @@
+-- Redirect command output to new unlisted-buffer
+vim.api.nvim_create_user_command("Redir", function(ctx)
+    local lines = vim.split(
+        vim.api.nvim_exec2(ctx.args, { output = true }).output,
+        "\n",
+        { plain = true }
+    )
+    vim.cmd("new")
+    vim.cmd("set nobuflisted")
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
+
 --Switch Case: PascalCase with snake_case
 function SwitchCase()
     local pos = vim.api.nvim_win_get_cursor(0)
