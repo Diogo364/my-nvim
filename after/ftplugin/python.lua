322ddf8375617c0683e0edfe49f48e0f7813533c
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2025-12-18 13:58:30 -0300

    Update Jupyter-like keybinding for python

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,6 @@
 local divider = "# %%"
-local divider_pattern = "# %%%%"
+local markdown_divider_suffix = " - [markdown]"
+local divider_pattern = "^# %%%%" -- Matches '# %%' at the start of a line
 local divider_hl_group_name = "PythonDivider"
 local namespace_id = vim.api.nvim_create_namespace(divider_hl_group_name)
 local group_python_divider =
@@ -12,115 +13,136 @@ vim.api.nvim_set_hl(0, divider_hl_group_name, {
     ctermfg = "Gray",
 })
 
-local highlight_divider_line = function()
-    -- Delete all previous extmarks
-    for _, extmark in
-        pairs(vim.api.nvim_buf_get_extmarks(0, namespace_id, 0, -1, {}))
-    do
-        vim.api.nvim_buf_del_extmark(0, namespace_id, extmark[1])
-    end
+local function highlight_dividers()
+    vim.api.nvim_buf_clear_namespace(0, namespace_id, 0, -1)
 
-    -- Define new extmarks
     for i, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
-        local match = string.match(line, divider_pattern)
-        if match ~= nil then
-            vim.api.nvim_buf_set_extmark(
-                0,
-                namespace_id,
-                i - 1,
-                0,
-                { line_hl_group = divider_hl_group_name }
-            )
+        if string.match(line, divider_pattern) then
+            vim.api.nvim_buf_set_extmark(0, namespace_id, i - 1, 0, {
+                line_hl_group = divider_hl_group_name,
+            })
         end
     end
 end
 
-vim.api.nvim_create_autocmd({ "BufEnter", "TextChangedI", "TextChanged" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI" }, {
     group = group_python_divider,
     pattern = "*.py",
-    callback = highlight_divider_line,
+    callback = highlight_dividers,
+    desc = "Highlight Python cell dividers",
 })
 
-local write_divider = function(line, is_markdown)
-    is_markdown = is_markdown or false
-    local curr_divider = divider
-    if is_markdown then
-        curr_divider = curr_divider .. " [markdown]"
-    end
-    local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
-    local target_line = cursor_line + line
+local function insert_divider(line_nr, is_markdown)
+    local line_content = is_markdown and (divider .. markdown_divider_suffix)
+        or divider
     vim.api.nvim_buf_set_lines(
         0,
-        target_line,
-        target_line,
+        line_nr - 1,
+        line_nr - 1,
         false,
-        { curr_divider }
+        { line_content }
     )
 end
 
-local find_next_divider = function()
-    vim.cmd(string.format("/%s", divider))
+local function find_next_divider_line()
+    return vim.fn.search(divider, "W")
 end
 
-local find_prev_divider = function()
-    vim.cmd(string.format("?%s", divider))
-end
-
-local enter_next_cell = function()
-    find_next_divider()
-    vim.cmd("norm j")
-end
-
-local enter_prev_cell = function()
-    find_prev_divider()
-    find_prev_divider()
-    vim.cmd("norm j")
+local function find_prev_divider_line()
+    return vim.fn.search(divider, "bW")
 end
 
 vim.keymap.set("n", "<leader>cs", function()
-    write_divider(-1)
-end, { buffer = true, desc = "Split current cell" })
+    local cursor_row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    insert_divider(cursor_row, false)
+    vim.api.nvim_win_set_cursor(0, { cursor_row, 0 })
+end, { buffer = true, desc = "Create code divider at cursor" })
 
-vim.keymap.set("n", "<leader>ct", function()
-    find_next_divider()
-    write_divider(-1, true)
-    vim.cmd("norm O")
-    vim.api.nvim_put({"# "}, "", false, true)
-    vim.cmd("startinsert!")
-end, { buffer = true, desc = "Split current cell" })
+vim.keymap.set("n", "<leader>cm", function()
+    local cursor_row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    insert_divider(cursor_row, true)
+    vim.api.nvim_win_set_cursor(0, { cursor_row + 1, 0 })
+    vim.cmd("normal! o# ")
+    vim.cmd("startinsert")
+end, { buffer = true, desc = "Create markdown cell at cursor" })
+
+vim.keymap.set("n", "<leader>cO", function()
+    local prev_divider_line = find_prev_divider_line()
+    if prev_divider_line == 0 then
+        prev_divider_line = 1
+    end
+
+    insert_divider(prev_divider_line, false)
+    vim.api.nvim_win_set_cursor(0, { prev_divider_line, 0 })
+    vim.cmd("normal! O")
+    vim.cmd("startinsert")
+end, { buffer = true, desc = "Create new code cell above" })
+
+vim.keymap.set("n", "<leader>co", function()
+    local next_divider_line = find_next_divider_line()
+    if next_divider_line == 0 then
+        next_divider_line = vim.api.nvim_buf_line_count(0) + 1
+    end
+    insert_divider(next_divider_line, false)
+    vim.api.nvim_win_set_cursor(0, { next_divider_line, 0 })
+    vim.cmd("normal! o")
+    vim.cmd("startinsert")
+end, { buffer = true, desc = "Create new code cell below" })
 
 vim.keymap.set("n", "<leader>cT", function()
-    find_prev_divider()
-    write_divider(-1, true)
-    vim.cmd("norm O")
-    vim.api.nvim_put({"# "}, "", false, true)
-    vim.cmd("startinsert!")
-end, { buffer = true, desc = "Split current cell" })
+    local prev_divider_line = find_prev_divider_line()
+    if prev_divider_line == 0 then
+        prev_divider_line = 1
+    end
+
+    insert_divider(prev_divider_line, true)
+    vim.api.nvim_win_set_cursor(0, { prev_divider_line, 0 })
+    vim.cmd("normal! o# ")
+    vim.cmd("startinsert")
+end, { buffer = true, desc = "Create new markdown cell above" })
+
+vim.keymap.set("n", "<leader>ct", function()
+    local next_divider_line = find_next_divider_line()
+    if next_divider_line == 0 then
+        next_divider_line = vim.api.nvim_buf_line_count(0) + 1
+    end
+    insert_divider(next_divider_line, true)
+    vim.api.nvim_win_set_cursor(0, { next_divider_line, 0 })
+    vim.cmd("normal! o# ")
+    vim.cmd("startinsert")
+end, { buffer = true, desc = "Create new markdown cell below" })
+
+local enter_next_cell = function()
+    local next_divider = find_next_divider_line()
+    if next_divider ~= 0 then
+        vim.api.nvim_win_set_cursor(0, { next_divider + 1, 0 })
+    end
+end
+
+local enter_prev_cell = function()
+    local prev_divider = find_prev_divider_line()
+    if prev_divider == 0 then
+        return
+    end
+
+    vim.api.nvim_win_set_cursor(0, { prev_divider, 0 })
+    local new_prev_divider = find_prev_divider_line()
+    if new_prev_divider == 0 then
+        vim.api.nvim_win_set_cursor(0, { 1, 0 })
+    else
+        vim.api.nvim_win_set_cursor(0, { new_prev_divider + 1, 0 })
+    end
+end
 
 vim.keymap.set(
     "n",
     "<leader>J",
     enter_next_cell,
-    { buffer = true, desc = "Split current cell" }
+    { buffer = true, desc = "Enter next cell" }
 )
-
 vim.keymap.set(
     "n",
     "<leader>K",
     enter_prev_cell,
-    { buffer = true, desc = "Split current cell" }
+    { buffer = true, desc = "Enter previous cell" }
 )
-
-vim.keymap.set("n", "<leader>cO", function()
-    write_divider(-1)
-    find_prev_divider()
-    vim.cmd("norm O")
-    vim.cmd("startinsert")
-end, { buffer = true, desc = "Split current cell" })
-
-vim.keymap.set("n", "<leader>co", function()
-    write_divider(0)
-    find_next_divider()
-    vim.cmd("norm o")
-    vim.cmd("startinsert")
-end, { buffer = true, desc = "Split current cell" })
